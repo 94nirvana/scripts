49f56c3c1d282f71fdb7e42e8bf056dca94f2281
Roberto Prado <104590113+94nirvana@users.noreply.github.com>
2022-04-30 17:41:50 -0300

    Update clientTCP.py

--- clientTCP.py
+++ clientTCP.py
@@ -5,9 +5,9 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.settimeout(5)
 
 try:
-    client.connect(("191.252.51.2", 80))
+    client.connect(("ip", 80))
 
-    client.send('GET / HTTP/1.1\nHost: ambientec.com\n\n\n')
+    client.send('GET / HTTP/1.1\nHost: host.com\n\n\n')
 
     print client.recv(1024)
 
